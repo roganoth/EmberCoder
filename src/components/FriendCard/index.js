895d5d1bdc47a392462b5e302304ebe05cec94cc
dan <chronos937@gmail.com>
2020-02-08 12:37:48 -0500

    adjusted card styles to eliminate text and removed unnecessary info from the json object

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -5,24 +5,13 @@ function FriendCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+        <img
+          alt={props.name}
+          src={props.image}
+          onClick={() => props.removeFriend(props.id)}
+          className="remove"
+        />
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
     </div>
   );
 }
