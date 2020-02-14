96bf332d15fb759ec2e8c40ea7597659c4743f0d
dan <chronos937@gmail.com>
2020-02-13 19:59:26 -0500

    Updates

---
+++ precache-manifest.db48c85da33aad211949ba621d8029e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c77ce4ad96da8bbbc7193c1622cc636",
+    "url": "/EmberCoder/index.html"
+  },
+  {
+    "revision": "b45116b828c12d42d604",
+    "url": "/EmberCoder/static/css/main.5362975d.chunk.css"
+  },
+  {
+    "revision": "79123dcbbbbbdba52a15",
+    "url": "/EmberCoder/static/js/2.2201ad36.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/EmberCoder/static/js/2.2201ad36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45116b828c12d42d604",
+    "url": "/EmberCoder/static/js/main.723af0aa.chunk.js"
+  },
+  {
+    "revision": "87a00e18d5ffcc8b68dd",
+    "url": "/EmberCoder/static/js/runtime-main.d8540102.js"
+  }
+]);
